a17d660b0b64a85ac104881ad7c551a756e8a1fc
crelder <crelder@github.com>
2018-08-20 19:28:11 +0200

    Remove Offline-Workaround

--- recipes.js
+++ recipes.js
@@ -1,85 +1,22 @@
-/* Read JSON-File from Server */
-// let recipesJSON;
-// let xmlhttp = new XMLHttpRequest();
-// xmlhttp.onreadystatechange = function() {
-//   if (this.readyState == 4 && this.status == 200) {
-//     recipesJSON = JSON.parse(this.responseText);
-//   }
-// };
-// xmlhttp.open(
-//   "GET",
-//   "https://rawgit.com/crelder/recipe-shoppinglist-generator/master/recipes.json",
-//   false
-// );
-// xmlhttp.send();
-/* To-Do: Changed JSON Format to be better readible: adapt the functions!  */
-
-// Random order of recipes
-// const recipeCollection = recipesJSON.sort(function() {
-//   return 0.5 - Math.random();
-// });
-/* --------------------------- */
-
-const recipeCollection = {
-  "Rucola Salat": {
-    ingredients: {
-      Parmesan: {
-        amount: 0.5,
-        unit: "Packung",
-        department: "Kühlregal"
-      },
-      "Rucula Salat": {
-        amount: 1,
-        unit: "Packung",
-        department: "Obst- und Gemüse"
-      },
-      Tomaten: {
-        amount: 1,
-        unit: "",
-        department: "Obst- und Gemüse"
-      }
-    },
-    comment:
-      "Parmesan in hauchdünne Scheiben mit Reibe schaben. Cocktail Tomaten sehen gut darauf aus."
-  },
-
-  "Griechischer Salat": {
-    ingredients: {
-      Gurke: {
-        amount: 1,
-        unit: "",
-        department: "Obst- und Gemüse"
-      },
-      "Rucula Salat": {
-        amount: 1,
-        unit: "Packung",
-        department: "Obst- und Gemüse"
-      },
-      Tomaten: {
-        amount: 4,
-        unit: "",
-        department: "Obst- und Gemüse"
-      },
-      Schafskäse: {
-        amount: 1,
-        unit: "Packung",
-        department: "Kühlregal"
-      },
-      Oliven: {
-        amount: 12,
-        unit: "",
-        department: "Haltbar"
-      },
-      Zwiebel: {
-        amount: 1,
-        unit: "",
-        department: "Obst- und Gemüse"
-      }
-    },
-    comment:
-      "ingredients in dicke Scheiben schneiden. Griechenland-Style! Zwiebeln in Ringe schneiden und etwas Salz darüber und kurz stehen lassen. Dann werden die weicher und sind nicht mehr so scharf."
+// Read JSON-File from Server
+let recipesJSON;
+let xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    recipesJSON = JSON.parse(this.responseText);
   }
 };
+xmlhttp.open(
+  "GET",
+  "https://rawgit.com/crelder/recipe-shoppinglist-generator/master/recipes.json",
+  false
+);
+xmlhttp.send();
+
+// Random order of recipes
+const recipeCollection = recipesJSON.sort(function() {
+  return 0.5 - Math.random();
+});
 
 function createUniqueIngredients(recipes) {
   const ings = {};
@@ -99,7 +36,6 @@ function createUniqueIngredients(recipes) {
       }
     }
   }
-  // console.log(JSON.stringify(ings, "", 3));
   return ings;
 }
 
@@ -210,8 +146,7 @@ var vm = new Vue({
         createIngredientStringforClipboard(selectedRecipesFull)
       );
 
-      return "Hello";
-      // return createIngredientStringforClipboard(selectedRecipesFull);
+      return createIngredientStringforClipboard(selectedRecipesFull);
     },
     generateMenuString() {
       this.menuString = createMenueStringforClipboard(this.selectedRecipes);
@@ -223,19 +158,4 @@ var vm = new Vue({
   }
 });
 
-//  Füge noch Allgäuer Käsesuppe hinzu mit Schnittlauch, sowie Sauerbraten, Rezept von Stephan MG (Tomatensalat mit Basilikum mit Nudeln), Rezept von Peter MG (Erbsen Suppe)
-
-// Erbsensuppe (von Peter)
-// 500g grüne Erbsen (tiefgekühlt oder frisch)
-// 1 Zwiebel gewürfelt
-// 30g Butter
-// 5 dl Bouillon
-// 100g Joghurt nature
-// 200g Crème fraîche
-// 1 EL Currypulver
-// 1 TL Garam Masala
-// Frischer Koriander (Peter spezial, steht nicht im Originalrezept)
-
-// Zwiebeln glasig braten. Erbsen hinzufügen, kurz andünsten. Mit Bouillon ablöschen, 10 Min. zugedeckt köcheln lassen. Pürieren (wir haben das mit unserem Smoothie-Mixer gemacht).
-// Restliche Zutaten (4 EL zurückbehalten als Tupfer obendrauf, eigentlich völlig unnötig, aber aus Sicht des Liebhabers eine wesentliche Verschönerung) vermischen, glattrühren, zu Erbsen geben, 1x aufkochen.
-// Koriander dazustellen.
+//  Füge noch Allgäuer Käsesuppe hinzu mit Schnittlauch, sowie Sauerbraten, Rezept von Stephan (Tomatensalat mit Basilikum mit Nudeln)
