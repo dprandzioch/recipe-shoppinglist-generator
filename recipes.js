bfe1cd289eb7b8d6c95c1028effd1305d3168ba2
crelder <crelder@github.com>
2018-08-28 18:37:01 +0200

    Recipe Fixes

--- recipes.js
+++ recipes.js
@@ -13,57 +13,60 @@
 // );
 // xmlhttp.send();
 
+//// console.log("document.location.href: ", document.location.href);
+// Retrieve JSON File Location from URL Ending ?=
+
 let recipesLocal = {
   "Rucola Salat": {
-    "ingredients": {
-      "Parmesan": {
-        "amount": 0.5,
-        "unit": "Packung",
-        "department": "Kühlregal"
+    ingredients: {
+      Parmesan: {
+        amount: 0.5,
+        unit: "Packung",
+        department: "Kühlregal"
       },
       "Rucola Salat": {
-        "amount": 1,
-        "unit": "Packung",
-        "department": "Obst- und Gemüse"
+        amount: 1,
+        unit: "Packung",
+        department: "Obst- und Gemüse"
       },
-      "Tomaten": {
-        "amount": 1,
-        "unit": "",
-        "department": "Obst- und Gemüse"
+      Tomaten: {
+        amount: 1,
+        unit: "",
+        department: "Obst- und Gemüse"
       }
     },
-    "comment":
+    comment:
       "Parmesan in hauchdünne Scheiben mit Reibe schaben. Cocktail Tomaten sehen gut darauf aus."
   },
   "Griechischer Salat": {
-    "ingredients": {
-      "Gurke": {
-        "amount": 1,
-        "unit": "",
-        "department": "Obst- und Gemüse"
+    ingredients: {
+      Gurke: {
+        amount: 1,
+        unit: "",
+        department: "Obst- und Gemüse"
       },
-      "Tomaten": {
-        "amount": 4,
-        "unit": "",
-        "department": "Obst- und Gemüse"
+      Tomaten: {
+        amount: 4,
+        unit: "",
+        department: "Obst- und Gemüse"
       },
-      "Schafskäse": {
-        "amount": 1,
-        "unit": "Packung",
-        "department": "Kühlregal"
+      Schafskäse: {
+        amount: 1,
+        unit: "Packung",
+        department: "Kühlregal"
       },
-      "Oliven": {
-        "amount": 12,
-        "unit": "",
-        "department": "Haltbar"
+      Oliven: {
+        amount: 12,
+        unit: "",
+        department: "Haltbar"
       },
-      "Zwiebel": {
-        "amount": 1,
-        "unit": "",
-        "department": "Obst- und Gemüse"
+      Zwiebel: {
+        amount: 1,
+        unit: "",
+        department: "Obst- und Gemüse"
       }
     },
-    "comment":
+    comment:
       "ingredients in dicke Scheiben schneiden. Griechenland-Style! Zwiebeln in Ringe schneiden und etwas Salz darüber und kurz stehen lassen. Dann werden die weicher und sind nicht mehr so scharf."
   }
 };
@@ -75,14 +78,10 @@ console.log(recipesLocal);
 //   return 0.5 - Math.random();
 // });
 
-// const recipeCollection = JSON.parse(recipesLocal);
-
-
 // Kann gelöscht werden
-// console.log("document.location.href: ", document.location.href);
 // console.log("JSON-Objekt", JSON.stringify(recipeCollection, "", 3));
 // console.log("JSON-Objekt", recipeCollection);
-
+console.log("Object.keys(recipeCollection)", Object.keys(recipeCollection));
 
 function createUniqueIngredients(recipes) {
   const ings = {};
@@ -130,33 +129,40 @@ function createIngredientStringforClipboard(recipes) {
   )}`;
 }
 
-function createMenueStringforClipboard(recipes) {
+function createMenueStringforClipboard(selectedRecipes, allFullRecipes, header) {
   date = new Date();
-  console.log("createMenueStringforClipboard");
-  // hier einfach noch einen Header übergeben
-  let menuString = `Menüliste ab dem ${date.toLocaleDateString("de-DE", {
-    weekday: "short",
-    year: "numeric",
-    month: "long",
-    day: "numeric"
-  })}:
-        ${Object.keys(recipes)
-          .join(", ")
-          .toUpperCase()}\n\n`;
+  console.log("selectedRecipes: ", selectedRecipes);
+  // Hier rausnehmen und einfach als Header-Variable übergeben
+  let menuString = header;
+  //String: Overview of selected recipes
+  menuString += selectedRecipes.join(", ").toUpperCase();
+  // Add complete selected recipes
+  let allFullRecipesArray = [];
+  for (recipeName in allFullRecipes) {
+      allFullRecipesArray.push(allFullRecipes[recipeName], );
+      // Add all these full recipes, that are in the selectedRecipes Array
+      // selectedRecipesFull.push(allFullRecipes.get(recipe));
+    };
+    console.log("allFullRecipesArray: ",allFullRecipesArray);
+  
+  // console.log("selectedRecipesFull", selectedRecipesFull);
 
-  for (var recipeName in recipes) {
-    menuString +=
-      recipeName.toUpperCase() + "\n" + "--------------------" + "\n";
-    for (var j = 0; j < recipes[i].ingredients.length; j++) {
-      menuString +=
-        recipes[i].ingredients[j].amount +
-        recipes[i].ingredients[j].unit +
-        " " +
-        recipes[i].ingredients[j].name +
-        "\n";
-    }
-    menuString += '"' + recipes[i].comment + '"' + "\n\n";
-  }
+  // menuString += JSON.stringify(allRecipes, "", 2);
+    // recipeCollection.filter(recipe => recipes.indexOf(Object.keys(recipe)));
+
+  // for (var recipeName in recipes) {
+  //   menuString +=
+  //     recipeName.toUpperCase() + "\n" + "--------------------" + "\n";
+  //   for (var j = 0; j < recipes[i].ingredients.length; j++) {
+  //     menuString +=
+  //       recipes[i].ingredients[j].amount +
+  //       recipes[i].ingredients[j].unit +
+  //       " " +
+  //       recipes[i].ingredients[j].name +
+  //       "\n";
+  //   }
+  //   menuString += '"' + recipes[i].comment + '"' + "\n\n";
+  // }
   return menuString;
 }
 
@@ -193,19 +199,18 @@ var vm = new Vue({
     return {
       recipes: recipeCollection,
       selectedRecipes: [],
-      menuString: ""
     };
   },
   methods: {
     onCopy: function(event) {
       alert(
         "Folgende Liste ist in die Zwischenablage kopiert:\n\n" + event.text
-      )
+      );
     },
-    
+
     generateIngredientString: function() {
       console.log("generateIngredientString Funktionsaufruf!");
-      //Warum wird bei jedem Klicken eines Rezeptes diese Funktion aufgerufen? 
+      //Warum wird bei jedem Klicken eines Rezeptes diese Funktion aufgerufen?
       // Weil sich mit jedem Klick selectedRecipes ändert und die Variable hier verwendet wird? Kann aber nicht sein...
       let selectedRecipesFull = {};
       this.selectedRecipes.forEach(
@@ -215,13 +220,18 @@ var vm = new Vue({
       return createIngredientStringforClipboard(selectedRecipesFull);
     },
     generateMenuString: function() {
-      this.menuString = createMenueStringforClipboard(this.selectedRecipes);
+      let header = `Menüliste ab dem ${date.toLocaleDateString("de-DE", {
+        weekday: "short",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+      })}\n`
+      return createMenueStringforClipboard(this.selectedRecipes, this.recipes, header);
     },
     onError: function() {
       alert("Fehler beim Kopieren in die Zwischenablage.");
     }
   }
 });
-
 
 //  Füge noch Allgäuer Käsesuppe hinzu mit Schnittlauch, sowie Sauerbraten
